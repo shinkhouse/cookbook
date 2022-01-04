1afdaeb2c2227d15e50070820763481964033efc
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-04 16:09:27 -0600

    add create recipe page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('./pages/recipes/recipes.module').then((m) => m.RecipesModule),
-  },
+    {
+        path: '',
+        loadChildren: () =>
+            import('./pages/recipes/recipes.module').then(
+                (m) => m.RecipesModule
+            ),
+    },
+    {
+        path: 'create',
+        loadChildren: () =>
+            import('./pages/create-recipe/create-recipe.module').then(
+                (m) => m.CreateRecipeModule
+            ),
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
